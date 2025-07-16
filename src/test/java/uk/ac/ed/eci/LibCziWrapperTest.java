ffb7ea5d0b787242191ebd629613a1aa65caba1c
Paul D Mitchell <paul.d.mitchell@ed.ac.uk>
2025-07-16 14:03:21 +0100

    Enable test image

--- src/test/java/uk/ac/ed/eci/LibCziWrapperTest.java
+++ src/test/java/uk/ac/ed/eci/LibCziWrapperTest.java
@@ -1,9 +1,16 @@
 package uk.ac.ed.eci;
 
 import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class LibCziWrapperTest {
+
+    private static final Path TEST_IMAGE_PATH = Paths.get("test-images", "test-image.czi");
 
     @Test
     public void testGetLibCZIVersionInfo() {
@@ -14,5 +21,14 @@ public class LibCziWrapperTest {
         // Add more assertions to validate the version information
     }
 
-    // Add more test methods for other functionalities
+    @Test
+    public void testOpenFileAndGetReader() {
+        assertTrue(Files.exists(TEST_IMAGE_PATH), "Test image should have been downloaded by Maven.");
+
+        LibCziFFM.InputStreamResult streamResult = LibCziFFM.createInputStreamFromFileUTF8(TEST_IMAGE_PATH.toString());
+        assertEquals(0, streamResult.errorCode(), "Should open stream without error.");
+        assertNotNull(streamResult.stream(), "Stream handle should not be null.");
+
+        // Add further tests using the opened stream...
+    }
 }
