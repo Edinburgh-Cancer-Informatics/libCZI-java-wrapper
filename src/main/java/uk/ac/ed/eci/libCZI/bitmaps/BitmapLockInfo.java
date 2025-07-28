3c0b6428ca559797b1a949c2905c7eebc1d80954
Paul D Mitchell <paul.d.mitchell@ed.ac.uk>
2025-07-28 15:20:36 +0100

    Pack 4 bytes structure

--- src/main/java/uk/ac/ed/eci/libCZI/bitmaps/BitmapLockInfo.java
+++ src/main/java/uk/ac/ed/eci/libCZI/bitmaps/BitmapLockInfo.java
@@ -5,6 +5,7 @@ import static java.lang.foreign.ValueLayout.*;
 import java.lang.foreign.Arena;
 import java.lang.foreign.MemoryLayout;
 import java.lang.foreign.MemorySegment;
+import java.lang.invoke.VarHandle;
 
 import uk.ac.ed.eci.libCZI.IInterop;
 
@@ -12,28 +13,36 @@ public class BitmapLockInfo implements IInterop, IBitmapLockInfo {
     private final MemorySegment ptrData;
     private final MemorySegment ptrDataRoi;
     private final int stride;
-    private final int size;
+    private final long size;
+
+    // Define the layout as a static final field
+    private static final MemoryLayout LAYOUT = MemoryLayout.structLayout(
+        ADDRESS.withName("ptrData").withByteAlignment(4),
+        ADDRESS.withName("ptrDataRoi").withByteAlignment(4),
+        JAVA_INT.withName("stride"), // Natural alignment is already 4
+        JAVA_LONG.withName("size").withByteAlignment(4));
+
+    // Create VarHandles for each field from the layout. This is the key to the fix.
+    private static final VarHandle PTR_DATA_HANDLE = LAYOUT.varHandle(MemoryLayout.PathElement.groupElement("ptrData"));
+    private static final VarHandle PTR_DATA_ROI_HANDLE = LAYOUT.varHandle(MemoryLayout.PathElement.groupElement("ptrDataRoi"));
+    private static final VarHandle STRIDE_HANDLE = LAYOUT.varHandle(MemoryLayout.PathElement.groupElement("stride"));
+    private static final VarHandle SIZE_HANDLE = LAYOUT.varHandle(MemoryLayout.PathElement.groupElement("size"));
 
     public static MemoryLayout layout() {
-        return MemoryLayout.structLayout(
-            ADDRESS.withName("ptrData"), // Not used
-            ADDRESS.withName("ptrDataRoi"),
-            JAVA_INT.withName("stride"),
-            // Align with padding if necessary (often is on 64-bit systems)
-            MemoryLayout.paddingLayout(4),                  
-            JAVA_LONG.withName("size"));
+        return LAYOUT;
     }
 
     public static BitmapLockInfo createFromMemorySegment(MemorySegment pBitmapLockInfo) {
+        // Use the VarHandles to get the data. This correctly handles the unaligned access.
         return new BitmapLockInfo(
-            pBitmapLockInfo.get(ADDRESS, layout().byteOffset(MemoryLayout.PathElement.groupElement("ptrData"))),
-            pBitmapLockInfo.get(ADDRESS, layout().byteOffset(MemoryLayout.PathElement.groupElement("ptrDataRoi"))),
-            pBitmapLockInfo.get(JAVA_INT, layout().byteOffset(MemoryLayout.PathElement.groupElement("stride"))),
-            pBitmapLockInfo.get(JAVA_INT, layout().byteOffset(MemoryLayout.PathElement.groupElement("size")))
+            (MemorySegment) PTR_DATA_HANDLE.get(pBitmapLockInfo, 0L),
+            (MemorySegment) PTR_DATA_ROI_HANDLE.get(pBitmapLockInfo, 0L),
+            (int) STRIDE_HANDLE.get(pBitmapLockInfo, 0L),
+            (long) SIZE_HANDLE.get(pBitmapLockInfo, 0L)
         );
     }
 
-    public BitmapLockInfo(MemorySegment ptrData, MemorySegment ptrDataRoi, int stride, int size) {
+    public BitmapLockInfo(MemorySegment ptrData, MemorySegment ptrDataRoi, int stride, long size) {
         this.ptrData = ptrData;
         this.ptrDataRoi = ptrDataRoi.reinterpret(size).asReadOnly();
         this.stride = stride;
@@ -44,7 +53,7 @@ public class BitmapLockInfo implements IInterop, IBitmapLockInfo {
         return stride;
     }
 
-    public int size() {
+    public long size() {
         return size;
     }
 
@@ -52,15 +61,14 @@ public class BitmapLockInfo implements IInterop, IBitmapLockInfo {
         return ptrDataRoi;
     }
 
-
     @Override
     public MemorySegment toMemorySegment(Arena arena) {
-        MemorySegment segment = arena.allocate(layout());
-        segment.set(ADDRESS, 0, MemorySegment.NULL);
-        segment.set(ADDRESS, 4, MemorySegment.NULL);
-        segment.set(JAVA_INT, 8, stride);
-        segment.set(JAVA_INT, 12, size);
+        MemorySegment segment = arena.allocate(LAYOUT);
+        // Use the VarHandles to set the data, ensuring correct alignment handling.
+        PTR_DATA_HANDLE.set(segment, 0L, ptrData);
+        PTR_DATA_ROI_HANDLE.set(segment, 0L, ptrDataRoi);
+        STRIDE_HANDLE.set(segment, 0L, stride);
+        SIZE_HANDLE.set(segment, 0L, size);
         return segment;
     }
-    
 }
