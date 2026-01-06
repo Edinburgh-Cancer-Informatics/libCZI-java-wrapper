e2c3617d771cf08aa4374bccd12b734a3ce8d71b
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2026-01-06 10:40:04 +0000

    Implement available dimensions

--- src/main/java/uk/ac/ed/eci/libCZI/document/AvailableDimensions.java
+++ src/main/java/uk/ac/ed/eci/libCZI/document/AvailableDimensions.java
@@ -1,5 +1,29 @@
 package uk.ac.ed.eci.libCZI.document;
 
+import java.lang.foreign.MemorySegment;
+import java.util.Arrays;
+
+
+import static java.lang.foreign.ValueLayout.JAVA_INT;
+
 public class AvailableDimensions {
-    
+
+    private final int[] availableDimensionArray;
+
+    private AvailableDimensions(int[] array) {
+        this.availableDimensionArray = array;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(availableDimensionArray);
+    }
+
+    public int[] toArray() {
+        return availableDimensionArray.clone();
+    }
+
+    public static AvailableDimensions createFromMemorySegment(MemorySegment segment) {;
+        return new AvailableDimensions(segment.toArray(JAVA_INT));
+    }
 }
