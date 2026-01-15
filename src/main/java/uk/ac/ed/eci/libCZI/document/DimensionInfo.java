834a0cc9cfb16719c65489d2019a17f189d71360
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2026-01-15 16:05:11 +0000

    Implement dimension info (#3)
    Update src/main/java/uk/ac/ed/eci/libCZI/document/DocumentInfo.java

--- src/main/java/uk/ac/ed/eci/libCZI/document/DimensionInfo.java
+++ src/main/java/uk/ac/ed/eci/libCZI/document/DimensionInfo.java
@@ -1,5 +1,15 @@
 package uk.ac.ed.eci.libCZI.document;
 
-public class DimensionInfo {
 
+public class DimensionInfo {
+    // JSON string, consumers can parse it
+    private final String string;
+
+    public DimensionInfo(String string) {
+        this.string = string;
+    }
+
+    public String getJSONString() {
+        return string;
+    }
 }
